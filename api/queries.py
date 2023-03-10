7a9a76739ed349516fdefbe27e2e8a255dfaa009
Nikita Gushtyn <nikita.hu@datarails.com>
2023-03-10 21:34:24 +0300

    completed base funcs

--- api/queries.py
+++ api/queries.py
@@ -3,9 +3,6 @@ from ariadne import convert_kwargs_to_snake_case
 from .models import Post
 
 
-
-
-
 @convert_kwargs_to_snake_case
 def getPost_resolver(obj, info, id):
     try:
@@ -14,9 +11,24 @@ def getPost_resolver(obj, info, id):
             "success": True,
             "post": post.to_dict()
         }
-    except AttributeError:  # todo not found
+    except AttributeError:
         payload = {
             "success": False,
             "errors": ["Post item matching {id} not found"]
+        }
+    return payload
+
+
+def listPosts_resolver(*args, **kwargs):
+    try:
+        posts = [p.to_dict() for p in Post.query.all()]
+        payload = {
+            "success": True,
+            "posts": posts
+        }
+    except Exception as error:
+        payload = {
+            "success": False,
+            "errors": [str(error)]
         }
     return payload
--- app.py
+++ app.py
@@ -1,48 +1,20 @@
 from ariadne import load_schema_from_path, make_executable_schema, graphql_sync, snake_case_fallback_resolvers, \
-    ObjectType, MutationType
+    ObjectType
 from ariadne.explorer.playground import PLAYGROUND_HTML
 from flask import request, jsonify
 
 from api import app
-from api.models import Post
-
-# from api.queries import listPosts_resolver, getPost_resolver
+from api.queries import listPosts_resolver, getPost_resolver
 
 query: ObjectType = ObjectType("Query")
 # mutation = MutationType()
 
-
-# query.set_field("listPosts", listPosts_resolver)
-# query.set_field("getPost", getPost_resolver)
-
-
-# @mutation.field("add_place")
-# def add_place(_, info, title, description):
-#     place = Post(title=title, description=description)
-#     place.save()
-#     return place.to_json()
-
-
-@query.field("posts")
-def listPosts_resolver(obj, info):
-    # try:
-    #     posts = [post.to_dict() for post in Post.query.all()]
-    #     print(posts)
-    #     payload = {
-    #         "success": True,
-    #         "posts": posts
-    #     }
-    # except Exception as error:
-    #     payload = {
-    #         "success": False,
-    #         "errors": [str(error)]
-    #     }
-    return {"id": 1, "title": "Paris", "description": "The city of lights", "country": "France", "created_at": 12312}
-
+query.set_field("getPosts", listPosts_resolver)
+query.set_field("getPost", getPost_resolver)
 
 type_defs = load_schema_from_path("schema.graphql")
 schema = make_executable_schema(
-    type_defs, snake_case_fallback_resolvers
+    type_defs, query, snake_case_fallback_resolvers
 )
 
 
@@ -58,7 +30,7 @@ def graphql_server():
         schema,
         data,
         context_value=request,
-        # debug=app.debug
+        debug=app.debug
     )
     status_code = 200 if success else 400
     return jsonify(result), status_code
@@ -66,22 +38,3 @@ def graphql_server():
 
 if __name__ == "__main__":
     app.run(debug=True)
-
-
-
-class Contact(models.Model):
-   first_name = models.CharField(max_length=150)
-   last_name = models.CharField(max_length=150)
-   phone = models.CharField(max_length=150)
-   email = models.CharField(max_length=150, blank=True)
-   created_at = models.DateTimeField(auto_now_add=True)
-
-
-def test_contact_create():
-   contact = Contact(first_name="John", last_name="Doe", phone="123456789")
-   contact.save()
-   assert contact.first_name == "John"
-   assert contact.last_name == "Doe"
-   assert contact.phone == "123456789"
-   assert contact.email == ""
-   assert contact.created_at is not None
